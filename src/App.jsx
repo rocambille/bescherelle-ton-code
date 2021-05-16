d00a9e64390eb5cd8e32fbd512c558d6e0b9a8f4
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2021-05-16 17:52:18 +0200

    fixed router for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -10,13 +10,13 @@ function App() {
   return (
     <main className="container mx-auto p-4 space-y-4 min-h-screen flex flex-col relative">
       <ContextProviders>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter>
           <Suspense fallback={<p>Chargement...</p>}>
             {pages.map(({ path, Component }, index) => (
               <Route key={index} exact path={path} component={Component} />
             ))}
           </Suspense>
-        </BrowserRouter>
+        </HashRouter>
       </ContextProviders>
     </main>
   );
